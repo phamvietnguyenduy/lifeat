e9aebcd97cdb3b71388f751c8507d6003591e477
phamvietnguyenduy <78025834+phamvietnguyenduy@users.noreply.github.com>
2023-01-13 17:39:14 +0700

    update 1

---
+++ src/assets/images/index.js
@@ -0,0 +1,24 @@
+
+const Images = {
+    face:  require('./face.png'),
+    list:  require('./list.png'),
+    lap: require('./lap.png'),
+    logo1: require('./logo1.png'),
+    logo2: require('./logo2.png'),
+    logo3: require('./logo3.png'),
+    logo4: require('./logo4.png'),
+    logo5: require('./logo5.png'),
+    bgimage: require('./bgimage.jpg'),
+    agif: require('./agif.gif'),
+    talk: require('./talk.png'),
+    circlebg: require('./circlebg.png'),
+    time: require('./time.png'),
+    chart: require('./chart.png'),
+    ig0: require('./ig1.jpeg'),
+    ig1: require('./ig3.jpeg'),
+    ig2: require('./ig4.jpeg'),
+    ig3: require('./ig2.jpeg'),
+    ig4: require('./ig5.jpeg'),
+    ig5: require('./ig6.jpeg'),
+}
+export default Images
---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import {FaDiscord,FaTwitter,FaTiktok} from 'react-icons/fa'
+import {RiInstagramFill} from 'react-icons/ri'
+const Footer = () => {
+  return (
+    <div>
+        <div className='px-20'>
+            <div className='flex justify-between items-start border-b-[1px] border-gray-400 py-9'>
+                <h1 className='text-black font-medium tracking-widest text-4xl'>LifeAt</h1>
+                <div className='logo flex justify-around w-[15%] items-start'>
+                    <FaDiscord size={33} className='bg-black text-white p-1 rounded-md hover:bg-gray-600'/>
+                    <FaTiktok size={33} className='bg-black text-white p-1 rounded-md hover:bg-gray-600'/>
+                    <FaTwitter size={33} className='bg-black text-white p-1 rounded-md hover:bg-gray-600'/>
+                    <RiInstagramFill size={33} className='bg-black text-white p-1 rounded-md hover:bg-gray-600'/>
+                </div>
+            </div>
+            <div className='pt-12 grid grid-cols-4 gap-x-18'>
+                <div>
+                    <p className='font-extrabold pb-2 text-base'>Product</p>
+                    <p className='text-base font-semibold text-gray-600 pt-3 hover:text-thBrick'>How LifeAt Works</p>
+                    <p className='text-base font-semibold text-gray-600 pt-3 hover:text-thBrick'>Use cases</p>
+                    <p className='text-base font-semibold text-gray-600 pt-3 hover:text-thBrick'>Pricing</p>
+                    <p className='text-base font-semibold text-gray-600 pt-3 hover:text-thBrick'>Help Center</p>
+                    <p className='text-base font-semibold text-gray-600 pt-3 hover:text-thBrick'>We are hiring!</p>
+                </div>
+                <div>
+                    <p className='font-extrabold pb-2'>Company</p>
+                    <p className='text-base font-semibold text-gray-600 pt-3 hover:text-thBrick'>About us</p>
+                    <p className='text-base font-semibold text-gray-600 pt-3 hover:text-thBrick'>Terms of use</p>
+                    <p className='text-base font-semibold text-gray-600 pt-3 hover:text-thBrick'>Privacy</p>
+                    <p className='text-base font-semibold text-gray-600 pt-3 hover:text-thBrick'>Give feedback</p>
+                </div>
+                <div>
+                    <p className='font-extrabold pb-2'>Contact Us</p>
+                    <p className='text-base font-semibold text-gray-600 pt-3 hover:text-thBrick'>Join our community</p>
+                    <p className='text-base font-semibold text-gray-600 pt-3 hover:text-thBrick'>Email us</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
