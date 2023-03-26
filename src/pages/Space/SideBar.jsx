4dfc7eb500154dac32dfe08aafe57ab0550b9f1a
phamvietnguyenduy <78025834+phamvietnguyenduy@users.noreply.github.com>
2023-03-26 14:23:17 +0700

    css fix

--- src/pages/Space/SideBar.jsx
+++ src/pages/Space/SideBar.jsx
@@ -89,126 +89,128 @@ const SideBar = ({ css }) => {
   };
   return (
     <div
-      className={`w-[21rem] ml-3 fixed top-3 glass rounded-md py-3 transition ease-in-out delay-150 duration-300`}
+      className={`w-[21rem] ml-3 fixed top-0 h-full pt-[10px] pb-[10px] rounded-md py-3 transition ease-in-out delay-150 duration-300`}
       style={{
         transform: `translate3d(${css.tx}rem,0px, 0px) scale3d(${css.sx}, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)`,
         transformStyle: "preserve-3d",
       }}
     >
-      <div className="Advertise bg-[#e9006b] px-2 py-2 rounded-md mx-3">
-        <div className="text-xs text-white font-medium flex justify-center py-3">
-          <span className="text-xl">💗</span>
-          Co-work with us this Valentine’s day! Join the room →
+      <div className="glass rounded-md py-3 h-full">
+        <div className="Advertise bg-[#e9006b] px-2 py-2 rounded-md mx-3">
+          <div className="text-xs text-white font-medium flex justify-center py-3">
+            <span className="text-xl">💗</span>
+            Co-work with us this Valentine’s day! Join the room →
+          </div>
         </div>
-      </div>
-      <div className="flex mt-3 justify-between mx-3">
-        <Link to="/explore">
-          <div className="bg-gray-100 rounded-md px-2 py-1 text-base text-thDark font-medium">
-            Explore 🔎{" "}
+        <div className="flex mt-3 justify-between mx-3">
+          <Link to="/explore">
+            <div className="bg-gray-100 rounded-md px-2 py-1 text-base text-thDark font-medium">
+              Explore 🔎{" "}
+            </div>
+          </Link>
+          <div className="text-base text-thDark font-circula">{time}</div>
+        </div>
+        <div className="Shuffle flex flex-col-reverse mx-3 mb-8">
+          <Shuffle />
+        </div>
+        <div className="glass mt-3 pb-4 mb-8">
+          <div className="flex py-4 pr-2 pl-8 justify-between">
+            <div>
+              <h3 className="text-base font-circula text-gray-600">
+                LifeAt Rainy HQ
+              </h3>
+              <p className="font-medium text-sm text-gray-400">Share Space →</p>
+            </div>
+            <div className="flex h-fit justify-center">
+              <div className="glass px-2 py-2 rounded-md mr-1">
+                {checkHeart ? (
+                  <BsHeartFill
+                    className="text-red-400"
+                    size={22}
+                    onClick={handleSave}
+                  />
+                ) : (
+                  <BsHeart
+                    className="text-red-400"
+                    size={22}
+                    onClick={handleSave}
+                  />
+                )}
+              </div>
+              <div className="glass px-2 py-2 rounded-md mr-1">
+                <RiMenuAddLine size={22} />
+              </div>
+            </div>
           </div>
-        </Link>
-        <div className="text-base text-thDark font-circula">{time}</div>
-      </div>
-      <div className="Shuffle flex flex-col-reverse mx-3 mb-8">
-        <Shuffle />
-      </div>
-      <div className="glass mt-3 pb-4 mb-8">
-        <div className="flex py-4 pr-2 pl-8 justify-between">
-          <div>
-            <h3 className="text-base font-circula text-gray-600">
-              LifeAt Rainy HQ
-            </h3>
-            <p className="font-medium text-sm text-gray-400">Share Space →</p>
-          </div>
-          <div className="flex h-fit justify-center">
-            <div className="glass px-2 py-2 rounded-md mr-1">
-              {checkHeart ? (
-                <BsHeartFill
-                  className="text-red-400"
-                  size={22}
-                  onClick={handleSave}
+          <div className="flex justify-center items-center pr-7 pl-8 py-2">
+            <div>
+              {volume > 0 ? (
+                <MdVolumeUp
+                  className="text-thDark"
+                  size={25}
+                  onClick={handleVolumeoff}
                 />
               ) : (
-                <BsHeart
-                  className="text-red-400"
-                  size={22}
-                  onClick={handleSave}
+                <MdVolumeOff
+                  className="text-thDark"
+                  size={25}
+                  onClick={handleVolumeoff}
                 />
               )}
             </div>
-            <div className="glass px-2 py-2 rounded-md mr-1">
-              <RiMenuAddLine size={22} />
+            <div className="w-0">
+              <video
+                src={rainlight}
+                autoPlay
+                controls
+                loop
+                ref={speakerState}
+              ></video>
+            </div>
+            <input
+              type="range"
+              className="h-[2px] flex-1 ml-3"
+              onChange={(e) => {
+                handleVolume(e);
+              }}
+              value={volume}
+            />
+          </div>
+        </div>
+        <div className="ml-3 flex">
+          <div>
+            <img src={Images.book} alt="" className="w-11 h-11 rounded-full" />
+          </div>
+          <div className="ml-3">
+            <div className="flex">
+              <span className="bg-trans-text trans-text font-medium text-base">
+                @LifeAt
+              </span>
+              <p className="rounded-full ml-1 bg-blue-600 text-white text-[7px] font-extrabold flex items-center justify-center w-3 h-3">
+                ✓
+              </p>
+            </div>
+            <div className="flex ml-3 mt-1 text-thDark items-center ">
+              <RiInstagramFill size={19} />
+              <RiYoutubeFill size={20} className="ml-2" />
+              <FaTiktok size={15} className="ml-2" />
+              <FiLink className="ml-2" />
             </div>
           </div>
         </div>
-        <div className="flex justify-center items-center pr-7 pl-8 py-2">
-          <div>
-            {volume > 0 ? (
-              <MdVolumeUp
-                className="text-thDark"
-                size={25}
-                onClick={handleVolumeoff}
-              />
-            ) : (
-              <MdVolumeOff
-                className="text-thDark"
-                size={25}
-                onClick={handleVolumeoff}
-              />
-            )}
+        <div className="flex absolute bottom-[10px]">
+          <FaqBtn conTent={"Desktop app"} />
+          <FaqBtn conTent={"Showcase"} />
+          <FaqBtn conTent={"FAQ"} />
+          <div
+            className="border-[1px] w-fit px-3 py-2 rounded-md ml-2 flex justify-center items-center cursor-pointer"
+            onClick={onOffnotifi}
+          >
+            <p className="bg-gray-300 font-circula py-[5px] px-[5px] rounded-full"></p>
           </div>
-          <div className="w-0">
-            <video
-              src={rainlight}
-              autoPlay
-              controls
-              loop
-              ref={speakerState}
-            ></video>
-          </div>
-          <input
-            type="range"
-            className="h-[2px] flex-1 ml-3"
-            onChange={(e) => {
-              handleVolume(e);
-            }}
-            value={volume}
-          />
         </div>
+        <div>{checkNotifi && <Notification ref={notifiRef} />}</div>
       </div>
-      <div className="ml-3 flex">
-        <div>
-          <img src={Images.book} alt="" className="w-11 h-11 rounded-full" />
-        </div>
-        <div className="ml-3">
-          <div className="flex">
-            <span className="bg-trans-text trans-text font-medium text-base">
-              @LifeAt
-            </span>
-            <p className="rounded-full ml-1 bg-blue-600 text-white text-[7px] font-extrabold flex items-center justify-center w-3 h-3">
-              ✓
-            </p>
-          </div>
-          <div className="flex ml-3 mt-1 text-thDark items-center mb-40">
-            <RiInstagramFill size={19} />
-            <RiYoutubeFill size={20} className="ml-2" />
-            <FaTiktok size={15} className="ml-2" />
-            <FiLink className="ml-2" />
-          </div>
-        </div>
-      </div>
-      <div className="flex mt-14">
-        <FaqBtn conTent={"Desktop app"} />
-        <FaqBtn conTent={"Showcase"} />
-        <FaqBtn conTent={"FAQ"} />
-        <div
-          className="border-[1px] w-fit px-3 py-2 rounded-md ml-2 flex justify-center items-center cursor-pointer"
-          onClick={onOffnotifi}
-        >
-          <p className="bg-gray-300 font-circula py-[5px] px-[5px] rounded-full"></p>
-        </div>
-      </div>
-      <div>{checkNotifi && <Notification ref={notifiRef} />}</div>
     </div>
   );
 };
